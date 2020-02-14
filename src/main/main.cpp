7c336d76950758f400046b82c9cd24deadba065a
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2020-02-13 20:39:59 -0500

    Fixed the compile issuue on Windows

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,7 +4,14 @@
 #include <iostream>
 #include "MyGame.h"
 
-using namespace std;
+#ifdef _WIN32
+#include <windows.h>
+#undef main
+
+#ifdef __GNUC__
+#define _stdcall  __attribute__((stdcall))
+#endif
+
 
 int main(int argc, char** argv)
 {
@@ -14,3 +21,23 @@ int main(int argc, char** argv)
 	delete myGame;
 	return 0;
 }
+
+int _stdcall
+WinMain(struct HINSTANCE__* hInstance,
+	struct HINSTANCE__* hPrevInstance,
+	char* lpszCmdLine,
+	int                 nCmdShow)
+{
+	return main(__argc, __argv);
+}
+
+#else
+int main(int argc, char** argv)
+{
+	MyGame* myGame = new MyGame();
+	myGame->start();
+
+	delete myGame;
+	return 0;
+}
+#endif
