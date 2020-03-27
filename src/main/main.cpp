16c956d7a8ea298d8a829a97ac61749f946ef213
David Chen <dzc5ta@virginia.edu>
2020-03-27 16:35:10 -0400

    added rooms to area2

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -17,7 +17,7 @@
 int main(int argc, char** argv)
 {
 	// comment out one of the following to choose between normal run and dev tool
-	//MyGame* myGame = new MyGame();
+	// MyGame* myGame = new MyGame();
 	DevTool* myGame = new DevTool();
 	myGame->start();
 	
@@ -40,9 +40,9 @@ int main(int argc, char** argv)
 {
 	// comment out one of the following to choose between normal run and dev tool
 	
-	MyGame* myGame = new MyGame();
+	// MyGame* myGame = new MyGame();
 
-	//DevTool* myGame = new DevTool();
+	DevTool* myGame = new DevTool();
 	myGame->start();
 
 	delete myGame;
